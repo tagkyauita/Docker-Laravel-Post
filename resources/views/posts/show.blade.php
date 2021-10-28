fc34db942aca9ac8149226b474573860cfa95a11
tagkyauita <yuta.jan.takagi@gmail.com>
2021-10-28 03:38:01 +0000

    first commit

---
+++ resources/views/posts/show.blade.php
@@ -0,0 +1,38 @@
+@extends('layout')
+
+@section('content')
+
+    <div class="container">
+        <div class="mt-4 text-end">
+        {!!Form::open(['route'=>['posts.destroy',$post->id],'method'=>'delete'])!!}
+            <a class="btn btn-primary" href="{{route('posts.edit',['post_id'=>$post->id])}}">
+                編集する
+            </a>
+            {!!Form::submit('削除する',['class'=>'btn btn-danger'])!!}
+        {!!Form::close()!!}
+        </div>
+    </div>
+
+    <div class="container my-4">
+
+        <div class="card mb-4">
+            <div class="card-header">
+                <h2>
+                    {{$post->title}}
+                </h2>
+            </div>
+            <div class="card-body">
+                <p class="card-text">
+                    {{$post->body}}
+                </p>
+            </div>
+            <div class="card-footer">
+                <span>
+                    投稿日時:{{$post->created_at->format('Y-m-d')}}
+                </span>
+            </div>
+        </div>
+
+    </div>
+
+@endsection('content')
